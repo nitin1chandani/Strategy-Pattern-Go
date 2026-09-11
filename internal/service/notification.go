9a9ff8a5eac9bce3db2bfe06de19c72def9e6073
nitin1chandani <nitin1chandani@gmail.com>
2026-09-11 18:39:32 +0530

    implemented strategy pattern

--- internal/service/notification.go
+++ internal/service/notification.go
@@ -1,18 +1,51 @@
 package service
 
-import (
-	"fmt"
-	constants "go-strategy-pattern/internal"
-)
+import "fmt"
 
-func Notify(notificationType, message string) {
-	if notificationType == string(constants.MAIL) {
-		fmt.Println(message)
-	}
-	if notificationType == string(constants.SMS) {
-		fmt.Println(message)
-	}
-	if notificationType == string(constants.PUSH_NOTIFICATION) {
-		fmt.Println(message)
-	}
+//raw manner
+// func Notify(notificationType, message string) {
+// 	if notificationType == string(constants.MAIL) {
+// 		fmt.Println(message)
+// 	}
+// 	if notificationType == string(constants.SMS) {
+// 		fmt.Println(message)
+// 	}
+// 	if notificationType == string(constants.PUSH_NOTIFICATION) {
+// 		fmt.Println(message)
+// 	}
+// }
+
+// Strategy
+// common contract for each notification type
+type NotificationStrategy interface {
+	Send(message string)
+}
+
+// concrete strategy
+type Email struct{}
+
+func (e Email) Send(message string) {
+	fmt.Println(message)
+}
+
+// concrete strategy
+type SMS struct{}
+
+func (s SMS) Send(message string) {
+	fmt.Println(message)
+}
+
+// concrete strategy
+type Push struct{}
+
+func (p Push) Send(message string) {
+	fmt.Println(message)
+}
+
+type Notify struct {
+	Strategy NotificationStrategy
+}
+
+func (n Notify) Send(message string) {
+	n.Strategy.Send(message)
 }
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"go-strategy-pattern/internal/service"
+	. "go-strategy-pattern/internal/service"
 )
 
 func main() {
 	fmt.Println("Practicing Strategy Pattern")
-	service.Notify("", "Hello Folks")
+	// service.Notify("", "Hello Folks")
+
+	notify := Notify{
+		Strategy: Email{},
+	}
+	message := "hello folks"
+	notify.Send(message)
 }
